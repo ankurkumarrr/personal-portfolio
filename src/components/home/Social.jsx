ea279145055b5ee0f79462bedcfdc6a9a1000466
Ankur Kumar <ankurkumarrr1@gmail.com>
2024-06-24 18:07:32 +0530

    home, socials

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+
+        <a href="github.com/ankurkumarrr" className="home__social-icon" target={'_blank'}>
+            <i className="uil uil-github"></i>
+        </a>
+
+        <a href="linkedin.com/in/ankurkumarrr/" className="home__social-icon" target={'_blank'}>
+            <i className="uil uil-linkedin"></i>
+        </a>
+
+    </div>
+  )
+}
+
+export default Social
